fbc31d91725056f3263f7e9cfe1ebcd837fd4bac
Tammarra McPhaul <shaemc22@gmail.com>
2021-12-26 16:27:15 -0500

    updated controllers

--- back-end/app/controllers/shows_controller.rb
+++ back-end/app/controllers/shows_controller.rb
@@ -3,49 +3,50 @@ class ShowsController < ApplicationController
 
   # GET /shows
   def index
-    @shows = Show.all
+    shows = Show.all
 
-    render json: @shows
+    render json: ShowSerializer.new(shows)
   end
 
   # GET /shows/1
   def show
-    render json: @show
+    render json: show
   end
 
   # POST /shows
   def create
-    @show = Show.new(show_params)
+    show = Show.new(show_params)
 
-    if @show.save
-      render json: @show, status: :created, location: @show
+    if show.save
+      render json: ShowSerializer.new(show), status: :created, location: show
     else
-      render json: @show.errors, status: :unprocessable_entity
+      render json: show.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /shows/1
   def update
-    if @show.update(show_params)
-      render json: @show
+    if show.update(show_params)
+      render json: show
     else
-      render json: @show.errors, status: :unprocessable_entity
+      render json: show.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /shows/1
   def destroy
-    @show.destroy
+    show = Show.find(params[:id])
+    show.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_show
-      @show = Show.find(params[:id])
+      show = Show.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def show_params
-      params.require(:show).permit(:title, :network, :year, :season, :image_link, :rating, :genre_id)
+      params.require(:show).permit(:title, :network, :year, :season, :image_link, :rating)
     end
 end
